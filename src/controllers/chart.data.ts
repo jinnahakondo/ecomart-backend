b9016fa6b9f33762de0fe8e44044b0cb56b3eab7
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-03-20 21:38:18 +0600

    daily revenue api added

--- src/controllers/chart.data.ts
+++ src/controllers/chart.data.ts
@@ -11,15 +11,46 @@ export const getChartData = async (req: Request, res: Response) => {
         },
       },
     ]);
+
+    // Monthly Revenue (Bar Chart)
+    const monthlyRevenue = await OrderModel.aggregate([
+      { $match: { status: "confirmed" } },
+      {
+        $group: {
+          _id: { $month: "$createdAt" },
+          revenue: { $sum: "$totalPrice" },
+          orders: { $sum: 1 },
+        },
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+    //Daily Revenue (Line Chart)
+    const dailyRevenue = await OrderModel.aggregate([
+      { $match: { status: "confirmed" } },
+      {
+        $group: {
+          _id: {
+            $dateToString: {
+              format: "%Y-%m-%d",
+              date: "$createdAt",
+            },
+          },
+          revenue: { $sum: "$totalPrice" },
+          orders: { $sum: 1 },
+        },
+      },
+    ]);
+
     res.status(200).json({
       statusData,
+      monthlyRevenue,
+      dailyRevenue,
     });
-  } catch (error) {
-    res.status(500).json(
-        {
-        message:"failed to get chart data",
-        statusData
-    }
-    )
+  } catch (error: any) {
+    res.status(500).json({
+      message: "failed to get chart data",
+      error: error.message,
+    });
   }
 };
