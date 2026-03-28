b59a7bc7b206e4387976a182a02056b8e1bdb3ab
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-03-28 22:43:38 +0600

    cors origin added for localhost

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ const app: Application = express();
 
 // Parsers
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 // Application routes
 // auth routes
