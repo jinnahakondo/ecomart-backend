9d681a17eae90b651af968f31dcfef6123a07647
mdjinnah akondo <mdjinnahakondo@gmail.com>
2026-03-31 13:29:23 +0600

    some update in review model

--- src/models/ReviewModel.ts
+++ src/models/ReviewModel.ts
@@ -7,35 +7,35 @@ interface IReview {
   avatar?: string;
   rating: number;
   comment: string;
-  createdAt: Date;
-  updatedAt: Date;
+  date: Date;
 }
 
 // Review Schema - stores user feedback
-const ReviewSchema = new mongoose.Schema<IReview>(
-  {
-    reviewerName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    avatar: String,
-    rating: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 5,
-    },
-    comment: {
-      type: String,
-      required: true,
-    },
+const ReviewSchema = new mongoose.Schema<IReview>({
+  reviewerName: {
+    type: String,
+    required: true,
   },
-  { timestamps: true },
-);
+  email: {
+    type: String,
+    required: true,
+  },
+  avatar: String,
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const ReviewModel =
   mongoose.models.Review || mongoose.model<IReview>("Review", ReviewSchema);
